e24056694e75c91eb40fb9108c52f3e7b43c65a7
LeeMontoya <jleemontoya@gmail.com>
2014-04-25 13:48:17 -0600

    ObjectPractice

---
+++ ObjectPractice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    
+    public class Bird
+    {
+        public int numberOfBirds;
+        public string color;
+        public string chirp;
+
+        public void chirp()
+        {
+
+        }
+        public void fly()
+        {
+
+        }
+        public void eat()
+        {
+            
+        }
+
+    }
+    
+    
+    }
+
+}
